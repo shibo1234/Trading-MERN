a5574979e001132760242395965ecb5d96ef0203
Shibo Zheng <burning_7l@hotmail.com>
2024-04-01 11:29:20 -0700

    update sth

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
-import About from "./pages/About"
-import SignUp from "./pages/SignUp"
-import Profile from "./pages/Profile"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './component/Header';
 
 export default function App() {
-  return<BrowserRouter>
-    <Route>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-    </Route>
-  </BrowserRouter>
+  return(
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
