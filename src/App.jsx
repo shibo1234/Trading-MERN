773a6ef73e7b2ea9d0c7b465277cd1a97dc82557
Shibo Zheng <burning_7l@hotmail.com>
2024-04-01 10:41:11 -0700

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 
 export default function App() {
-  return (
-    <h1 className='text-red-500' >
-      App
-    </h1>
-  )
+  return<BrowserRouter>
+    <Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Route>
+  </BrowserRouter>
 }
 
