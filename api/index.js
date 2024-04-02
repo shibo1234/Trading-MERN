0d6e9cbafeb83876f7b11bac8c34e4b9b33f4781
Shibo Zheng <burning_7l@hotmail.com>
2024-04-01 22:35:02 -0700

    add user auth

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from  'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './route/user.route.js';
+import authRouter from './route/auth.route.js';
 
 dotenv.config()
 
@@ -15,7 +17,13 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Servr is running on port 3000');
 });
+
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
