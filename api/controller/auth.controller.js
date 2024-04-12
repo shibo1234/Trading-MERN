5fbc67ad7f535937b89aa73a0f8133e4c7738170
Shibo Zheng <burning_7l@hotmail.com>
2024-04-12 14:59:41 -0700

    finish sign up

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,10 +1,16 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async(req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword});
+    if (!username || !email || !password) {
+        res.status(400).json({ message: "All fields are required" });
+        return;
+    }
+
     try {
         await newUser.save()
         res.status(201).json("User create successfully");
